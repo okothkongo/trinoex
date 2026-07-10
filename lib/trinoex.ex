73564224a891481c8b8927abcbc939b7db0e48b2
okoth kongo <okothkongo@gmail.com>
2026-07-10 21:05:31 +0300

    let there be trinoex

---
+++ lib/trinoex.ex
@@ -0,0 +1,18 @@
+defmodule Trinoex do
+  @moduledoc """
+  Documentation for `Trinoex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Trinoex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/trinoex_test.exs
@@ -0,0 +1,8 @@
+defmodule TrinoexTest do
+  use ExUnit.Case
+  doctest Trinoex
+
+  test "greets the world" do
+    assert Trinoex.hello() == :world
+  end
+end
